ec2964886e669a2e23ed0ed0ff5b5e1ece2daf70
Dale <ddal345@gmail.com>
2018-08-25 20:12:17 +1000

    Initial app
    This is the base app

---
+++ app.py
@@ -0,0 +1,31 @@
+import requests
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+
+@app.route('/', methods = ['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        city = request.form.get('city')
+    else:
+        city = 'Melbourne'
+
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=718c1bc1f10f39fa695cfdc69b08e3f8'
+
+    r = requests.get(url.format(city)).json()
+
+    weather = {
+        'city' : r['name'],
+        'country' : r['sys']['country'],
+        'temperature' : r['main']['temp'],
+        'description' : r['weather'][0]['description'],
+        'icon' : r['weather'][0]['icon']
+    }
+
+    return render_template('weather.html', weather=weather)
+
+# run app
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ tests.py
@@ -0,0 +1,30 @@
+from app import app
+import unittest
+
+
+class FlaskTestCase(unittest.TestCase):
+
+    # Ensure that flask was set up correctly
+    def test_index(self):
+        tester = app.test_client(self)
+        response = tester.get('/', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+    
+    # Ensure that page loads correctly
+    def test_page_loads(self):
+        tester = app.test_client(self)
+        response = tester.get('/', content_type='html/text')
+        self.assertTrue(b'Get weather' in response.data)
+    
+    # Ensure correct result appears
+    def test_weather_result(self):
+        tester = app.test_client(self)
+        response = tester.post('/', data=dict(city="Las Vegas"), follow_redirects = True)
+        self.assertTrue(b'US' in response.data)
+        self.assertFalse(b'AU' in response.data)
+    
+    
+
+
+if __name__ == '__main__':
+    unittest.main()
